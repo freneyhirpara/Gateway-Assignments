cb7c6b14ca6a2216b1eb5a60fd3734ee1c30a362
freneyhirpara <64154207+freneyhirpara@users.noreply.github.com>
2021-04-17 14:33:00 +0530

    Update Program.cs

--- Testing-Assignment/Testing_Assignment-2/Testing_Assignment-2/Program.cs
+++ Testing-Assignment/Testing_Assignment-2/Testing_Assignment-2/Program.cs
@@ -10,52 +10,52 @@ namespace TestingAssignment2
     {
         static void Main(string[] args)
         {
-            string input = Console.ReadLine();
+            string inputString = Console.ReadLine();
             string outputValue;
 
             //Case 1 : convert the given input string to uppercase
             
-            outputValue = input.Conversion("UpperToLower");
+            outputValue = inputString.UpperToLower();
             Console.WriteLine("UpperToLower : " + outputValue);
 
             //Case 2 : convert the given input string to titlecase
             
-            outputValue = input.Conversion("TitleCase");
+            outputValue = inputString.TitleCase();
             Console.WriteLine("TitleCase : " + outputValue);
 
             //Case 3 : convert the given input string to capitalized
             
-            outputValue = input.Conversion( "Capitalized");
+            outputValue = inputString.Capitalized();
             Console.WriteLine("Capitalized : " + outputValue);
 
             //Case 4 : check the given input string is in lowercase or not
             
-            outputValue = input.Conversion( "CheckLowerCase");
+            outputValue = inputString.CheckLowerCase();
             Console.WriteLine("CheckLowerCase : " + outputValue);
 
             //Case 5 : check the given input string is in uppercase or not
             
-            outputValue = input.Conversion("CheckUpperCase");
+            outputValue = inputString.CheckUpperCase();
             Console.WriteLine("CheckUpperCase : " + outputValue);
 
             //Case 6 : check the given input string is int or not
             
-            outputValue = input.Conversion("CheckforInt");
+            outputValue = inputString.CheckforInt();
             Console.WriteLine("CheckforInt : " + outputValue);
 
             //Case 7 : remove last character from the given input string
             
-            outputValue = input.Conversion("RemoveLastCharacter");
+            outputValue = inputString.RemoveLastCharacter();
             Console.WriteLine("RemoveLastCharacter : " + outputValue);
 
             //Case 8 : count the number of words in the given input string
             
-            outputValue = input.Conversion( "WordCount");
+            outputValue = inputString.WordCount();
             Console.WriteLine("WordCount : " + outputValue);
 
             //Case 9 convert the given input string to int type
             
-            outputValue = input.Conversion( "StringToInt");
+            outputValue = inputString.StringToInt();
             Console.WriteLine("StringToInt : " + outputValue);
             Console.ReadLine();
         }
